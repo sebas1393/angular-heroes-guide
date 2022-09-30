637e4e7e47f63bcd27f2612d5de8c3b2dbae4035
sebas1393 <47646175+sebas1393@users.noreply.github.com>
2022-09-29 22:39:59 -0500

    form control in cart component

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent implements OnInit {
   items = this.cartService.getItems();
-  constructor(
-    private cartService: CartService
-  ){}
 
-  ngOnInit(): void{}
-  
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit(): void {}
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submited!', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
